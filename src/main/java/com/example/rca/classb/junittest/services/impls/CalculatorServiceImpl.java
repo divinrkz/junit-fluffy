98533bff91a18f5e832e89a87e15dff4fed19031
DivinIrakiza <irakizadivin@gmail.com>
2021-11-03 10:46:39 +0200

    (chore): CalculatorServiceImplementation

---
+++ src/main/java/com/example/rca/classb/junittest/services/impls/CalculatorServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.rca.classb.junittest.services.impls;
+
+public class CalculatorServiceImpl {
+    public int calculateSum(int[] data) {
+        int sum = 0;
+        for (int value: data) {
+            sum += value;
+        }
+        return  sum;
+    }
+}
