fff71fece0ccd226fcd7ee194b8fdf88237ddedb
DivinIrakiza <irakizadivin@gmail.com>
2021-11-03 10:42:26 +0200

    (chore): Initial setup

---
+++ src/test/java/com/example/rca/classb/junittest/JunitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rca.classb.junittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
