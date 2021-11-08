c22bbe62894e2c6a3ffbbb645e32a41cc414f0f1
DivinIrakiza <irakizadivin@gmail.com>
2021-11-08 22:16:06 +0200

    (chore): getAll items()

---
+++ src/main/java/com/example/rca/classb/junittest/controllers/ItemController.java
@@ -0,0 +1,20 @@
+package com.example.rca.classb.junittest.controllers;
+
+import com.example.rca.classb.junittest.models.Item;
+import com.example.rca.classb.junittest.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController("/api/v1/items")
+public class ItemController {
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/all-items")
+    public List<Item> getAll(){
+        return itemService.getAll();
+    }
+}
